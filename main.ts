22bd29e6975466a9feda92c45d3d3d01b6b0e433
halaszgergely <114067887+halaszgergely@users.noreply.github.com>
2022-11-10 09:00:14 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,57 @@
+let hangero = 0
+while (true) {
+    if (input.buttonIsPressed(Button.B) && hangero < 5) {
+        hangero += 1
+    } else if (input.buttonIsPressed(Button.A) && 0 < hangero) {
+        hangero += -1
+    }
+    if (hangero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (hangero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # # . . .
+            `)
+    } else if (hangero == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+    } else if (hangero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            # # # # .
+            `)
+    } else if (hangero == 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+}
