58af3c0fc477f53372a90722b2ade036cbe9dd2c
halaszgergely <114067887+halaszgergely@users.noreply.github.com>
2022-11-10 09:01:46 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let hangero = 0
 while (true) {
-    if (input.buttonIsPressed(Button.B) && hangero < 5) {
+    if (input.buttonIsPressed(Button.B)) {
         hangero += 1
     } else if (input.buttonIsPressed(Button.A) && 0 < hangero) {
         hangero += -1
